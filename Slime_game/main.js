e57ca90b51c3da67728bd04315a281d9b5362f5d
canikostar99 <54637481+canikostar99@users.noreply.github.com>
2020-01-24 15:17:13 -0500

    Added Camera rotation on arrow keys
    Camera should move around cube

--- Slime_game/main.js
+++ Slime_game/main.js
@@ -15,7 +15,49 @@ var cubeGeometry = new THREE.CubeGeometry(15,15, 15);
 //var cubeMaterial = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('crate.jpg')});
 var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xddaa66});
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
- 
+var k37 =false;
+var k38 =false;
+var k39 =false;
+var k40 =false;
+
+function doKeyDown(event) {
+		var code = event.keyCode;
+	switch(code) {
+	case 37: // <
+			k37 = true;
+			break;
+		case 39: // >
+			k39=true;
+			break;
+		case 38: // ^
+			k38 = true;
+			break;
+		case 40: // v
+			k40 = true;
+			break;
+	
+	}
+	
+}
+
+function doKeyUp(event) {
+		var code = event.keyCode;
+	switch(code) {
+	case 37: // <
+			k37 = false;
+			break;
+		case 39: // >
+			k39=false;
+			break;
+		case 38: // ^
+			k38 = false;
+			break;
+		case 40: // v
+			k40 = false;
+			break;
+	}	
+}
+
  
 
 // create perspective camera
@@ -43,15 +85,53 @@ var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.
 var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
 scene.add(skybox);
  */
- 
+document.addEventListener('keyup', doKeyUp, false);
+document.addEventListener('keydown', doKeyDown, false);
 renderer.render(scene, camera);
 
 
 function render() {
+	var rotSpeed = 0.03;
+	var x = camera.position.x, y = camera.position.y, z = camera.position.z;
     renderer.render(scene, camera);
     requestAnimationFrame(render);
     cube.rotation.y += 0.01;
-	
+	if( k37 || k39){
+
+		if(k37){
+	camera.position.x = x * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
+    camera.position.z = z * Math.cos(rotSpeed) - x * Math.sin(rotSpeed);
+    camera.lookAt(scene.position);
+
+
+		}
+		if(k39){
+    camera.position.x = x * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
+    camera.position.z = z * Math.cos(rotSpeed) + x * Math.sin(rotSpeed);
+    camera.lookAt(scene.position);
+
+
+		}
+
+
+	}
+	if( k38 || k40){
+
+		if(k38){
+	camera.position.z = z * Math.cos(rotSpeed) + y * Math.sin(rotSpeed);
+    camera.position.y = y * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
+    camera.lookAt(scene.position);
+
+
+		}
+		if(k40){
+    camera.position.z = z * Math.cos(rotSpeed) - y * Math.sin(rotSpeed);
+    camera.position.y = y * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
+    camera.lookAt(scene.position);
+		}
+
+
+	}
 }
 render();
 
