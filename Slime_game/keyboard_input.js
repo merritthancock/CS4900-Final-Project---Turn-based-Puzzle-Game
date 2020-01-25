d647c165be193f28413efd9affee843e7c20997f
Alan Bettis <alanbettis56@gmail.com>
2020-01-25 16:29:25 -0500

    added keyDown value adjustments to keyboard_input.js

--- Slime_game/keyboard_input.js
+++ Slime_game/keyboard_input.js
@@ -1,22 +1,23 @@
 //Define variables to store status of each used key
-var leftArrow = false
-var upArrow = false
-var rightArrow = false
-var downArrow = false
+var leftArrow = false;
+var upArrow = false;
+var rightArrow = false;
+var downArrow = false;
 
-var qKey = false
-var eKey = false
+var qKey = false;
+var eKey = false;
 
-var wKey = false
-var aKey = false
-var sKey = false
-var dKey = false
+var wKey = false;
+var aKey = false;
+var sKey = false;
+var dKey = false;
 
 function doKeyDown(event) {
 	var code = event.keyCode;
 
 	switch(code) {
-		case 37: // <
+        //Cases for the arrow keys, currently bound to camera controls
+        case 37: // <
 			leftArrow = true;
 			break;
 		case 39: // >
@@ -27,7 +28,29 @@ function doKeyDown(event) {
 			break;
 		case 40: // v
 			downArrow = true;
-			break;
+            break;
+            
+        //Cases for the q and e keys
+        case 81: //q
+            qKey = true;
+            break;
+        case 69: //e
+            eKey = true;
+            break;
+
+        //Cases for WASD keys
+        case 87: //w
+            wKey = true;
+            break;
+        case 65: //a
+            aKey = true;
+            break;
+        case 83: //s
+            sKey = true;
+            break;
+        case 68: //d
+            dKey = true;
+            break;
 	}
 }
 
@@ -35,7 +58,8 @@ function doKeyUp(event) {
 	var code = event.keyCode;
 
 	switch(code) {
-		case 37: // <
+        //Cases for the arrow keys, currently bound to camera controls
+        case 37: // <
 			leftArrow = false;
 			break;
 		case 39: // >
@@ -46,6 +70,28 @@ function doKeyUp(event) {
 			break;
 		case 40: // v
 			downArrow = false;
-			break;
-	}	
+            break;
+            
+        //Cases for the q and e keys
+        case 81: //q
+            qKey = false;
+            break;
+        case 69: //e
+            eKey = false;
+            break;
+
+        //Cases for WASD keys
+        case 87: //w
+            wKey = false;
+            break;
+        case 65: //a
+            aKey = false;
+            break;
+        case 83: //s
+            sKey = false;
+            break;
+        case 68: //d
+            dKey = false;
+            break;
+	}
 }
