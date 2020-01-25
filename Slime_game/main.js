a3aa641614621a1062b24fc032d2766a9dff41c8
Alan Bettis <alanbettis56@gmail.com>
2020-01-25 14:52:43 -0500

    Changed key bool variable names to be more meaningful

--- Slime_game/main.js
+++ Slime_game/main.js
@@ -9,55 +9,12 @@ document.body.appendChild(renderer.domElement);
 // create scene object
 var scene = new THREE.Scene;
 
-var k37 = false;
-var k38 = false;
-var k39 = false;
-var k40 = false;
-
-function doKeyDown(event) {
-	var code = event.keyCode;
-
-	switch(code) {
-	case 37: // <
-			k37 = true;
-			break;
-		case 39: // >
-			k39=true;
-			break;
-		case 38: // ^
-			k38 = true;
-			break;
-		case 40: // v
-			k40 = true;
-			break;
-	}
-}
-
-function doKeyUp(event) {
-	var code = event.keyCode;
-
-	switch(code) {
-	case 37: // <
-			k37 = false;
-			break;
-		case 39: // >
-			k39=false;
-			break;
-		case 38: // ^
-			k38 = false;
-			break;
-		case 40: // v
-			k40 = false;
-			break;
-	}	
-}
-
 //used to determine player start position for the level
 var startPos = [1, 0.5, 1]; // 0, 0.5, 0 will put cube in back left corner of size 100, div 20 grid
 
 // create player and add to scene
 var cubeGeometry = new THREE.CubeGeometry(1,1,1);
-var cubeMaterial = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('slime.jpg')});
+var cubeMaterial = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('assets/slime.jpg')});
 var player = new THREE.Mesh(cubeGeometry, cubeMaterial);
 player.position.set(startPos[0], startPos[1], startPos[2]);
 console.log(player.position);
@@ -79,14 +36,11 @@ scene.add(pointLight);
 scene.add(player);
 
 //Set up the ground
-//grassland = new THREE.Mesh(new THREE.PlaneGeometry(200,200),
-//            new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true})
-//);
-//grassland.rotation.x -= Math.PI / 2;
-//grassland.position.set(0, 0, 0);
-//scene.add(grassland);
-
-//grassland.position.set(0,0,0);
+grassland = new THREE.Mesh(new THREE.PlaneGeometry(200,200),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true})
+);
+grassland.rotation.x -= Math.PI / 2;
+grassland.position.set(0, 0, 0);
 scene.add(grassland);
 
 //Set up grid
@@ -113,25 +67,25 @@ function render() {
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   
-	if( k37 || k39){
-		if(k37){
+	if( leftArrow || rightArrow){
+		if(leftArrow){
 			camera.position.x = x * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
 			camera.position.z = z * Math.cos(rotSpeed) - x * Math.sin(rotSpeed);
 			camera.lookAt(scene.position);
 		}
-		if(k39){
+		if(rightArrow){
 			camera.position.x = x * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
 			camera.position.z = z * Math.cos(rotSpeed) + x * Math.sin(rotSpeed);
 			camera.lookAt(scene.position);
 		}
 	}
-	if( k38 || k40){
-		if(k38){
+	if( upArrow || downArrow){
+		if(upArrow){
 			camera.position.z = z * Math.cos(rotSpeed) + y * Math.sin(rotSpeed);
 			camera.position.y = y * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
 			camera.lookAt(scene.position);
 		}
-		if(k40){
+		if(downArrow){
 			camera.position.z = z * Math.cos(rotSpeed) - y * Math.sin(rotSpeed);
 			camera.position.y = y * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
 			camera.lookAt(scene.position);
