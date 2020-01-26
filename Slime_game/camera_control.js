cdbab2968f98280d81a60238c827f04ce817e35b
canikostar99 <54637481+canikostar99@users.noreply.github.com>
2020-01-25 19:05:55 -0500

    Updating camera controls
    Un-inverted camera rotation and added camera reset

--- Slime_game/camera_control.js
+++ Slime_game/camera_control.js
@@ -1,23 +1,42 @@
 //rotate the camera about center of scene when called
 function rotateCamera(){
     if(leftArrow){
-        camera.position.x = x * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
-        camera.position.z = z * Math.cos(rotSpeed) - x * Math.sin(rotSpeed);
-        camera.lookAt(scene.position);
-    }
-    if(rightArrow){
         camera.position.x = x * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
         camera.position.z = z * Math.cos(rotSpeed) + x * Math.sin(rotSpeed);
         camera.lookAt(scene.position);
     }
-    if(upArrow){
-        camera.position.z = z * Math.cos(rotSpeed) + y * Math.sin(rotSpeed);
-        camera.position.y = y * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
-        camera.lookAt(scene.position);
+    if(rightArrow){
+        camera.position.x = x * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
+        camera.position.z = z * Math.cos(rotSpeed) - x * Math.sin(rotSpeed);
+        camera.lookAt(scene.position);        
     }
-    if(downArrow){
+    if(upArrow){
         camera.position.z = z * Math.cos(rotSpeed) - y * Math.sin(rotSpeed);
         camera.position.y = y * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
         camera.lookAt(scene.position);
+    }
+    if(downArrow){
+        camera.position.z = z * Math.cos(rotSpeed) + y * Math.sin(rotSpeed);
+        camera.position.y = y * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
+        camera.lookAt(scene.position);       
+    }
+}
+function zoomCamera(){
+    if(qKey){
+        camera.fov += 1;
+        camera.lookAt(scene.position);
+    }
+    if(eKey){
+        camera.fov -= 1;
+        camera.lookAt(scene.position);        
+    }
+}
+function resetCamera(){
+    if(rKey){
+        camera.position.x = 0;
+		camera.position.y = 10;
+		camera.position.z = 10;
+		camera.fov = 45;
+        camera.lookAt(player.position);
     }
 }
