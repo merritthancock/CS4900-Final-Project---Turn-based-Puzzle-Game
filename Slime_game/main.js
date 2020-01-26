fbbbbf967c282d1be624b14073a282c1708c33cb
Merritt Hancock <merrittdhancock@gmail.com>
2020-01-25 20:05:08 -0500

    Refactored code to set up grid and fixed some issues with cube

--- Slime_game/main.js
+++ Slime_game/main.js
@@ -1,23 +1,12 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
  
 // create scene object
 var scene = new THREE.Scene;
-
-//used to determine player start position for the level
-var startPos = [1, 0.5, 1]; // 0, 0.5, 0 will put cube in back left corner of size 100, div 20 grid
-
-// create player and add to scene
-var cubeGeometry = new THREE.CubeGeometry(1,1,1);
-var cubeMaterial = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('assets/slime.jpg')});
-var player = new THREE.Mesh(cubeGeometry, cubeMaterial);
-player.position.set(startPos[0], startPos[1], startPos[2]);
-console.log(player.position);
 
 // create perspective camera
 var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
@@ -29,11 +18,10 @@ scene.add(camera);
 // create the view matrix
 camera.lookAt(player.position);
 
-// add lighting and add to scene 
+// create lighting and add to scene 
 var pointLight = new THREE.PointLight(0xaabbcc);
 pointLight.position.set(10, 16, 16);
 scene.add(pointLight);
-
 
 //add player to scene and set start position
 scene.add(player);
@@ -48,6 +36,8 @@ grassland.position.set(0, 0, 0);
 scene.add(grassland);
 
 //Set up grid
+createGrid1();
+/*
 function setGrid(size, divisions){
     this.size = size;
     this.divisions = divisions;
@@ -55,7 +45,8 @@ function setGrid(size, divisions){
     var gridHelper = new THREE.GridHelper( size, divisions);
     scene.add( gridHelper );
 }
-setGrid(100, 20);
+setGrid(100, 20);*/
+
 
 //Set up the skybox
 var skyboxGeometry = new THREE.CubeGeometry(1000, 1000, 1000);
@@ -63,6 +54,7 @@ var skyboxMaterial = new THREE.MeshBasicMaterial({  color: 0xffffff, side: THREE
 var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
 scene.add(skybox);
 
+//Add event listeners
 document.addEventListener('keyup', doKeyUp, false);
 document.addEventListener('keydown', doKeyDown, false);
 renderer.render(scene, camera);
