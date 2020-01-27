9c69d527ca83e605c5abc49381a3d3aec6419d1e
Ryan Trull <ryantrull@ryans-mbp.lan>
2020-01-26 21:07:35 -0500

    Deleted to backup

--- Slime_game/models.js
+++
@@ -1,8 +0,0 @@
-//Start position of player
-// -90, 7.5, -90 will put cube in back left corner of size 100, div 20 grid
-var startPos = [1, 0.5, 1];
-
-//Create cube to represent player/slime
-var cubeGeometry = new THREE.CubeGeometry(1,1,1);
-var cubeMaterial = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('./assets/slime.jpg')});
-var player = new THREE.Mesh(cubeGeometry, cubeMaterial);
