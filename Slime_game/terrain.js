89a4792c2e1f6e276867578639581f169dfe0f51
Ryan Trull <ryantrull@Ryans-MBP.lan>
2020-01-31 18:01:57 -0500

    renamed tnorm to terrain

--- Slime_game/terrain.js
+++ Slime_game/terrain.js
@@ -1,10 +1,12 @@
 function zTerrain(id){ //Zero terrain (death pitfall)
     var id = -1;
+    var traversable = true;
     //to be implemented
 }
 
 function nTerrain(x, z){ //Normal terrain (no restrictions)
     var id = 0;
+    var traversable = true;
     grassN = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as grass
              new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/grass.jpg') })
     );
@@ -15,6 +17,7 @@ function nTerrain(x, z){ //Normal terrain (no restrictions)
 
 function uTerrain(x, z){ //Untraversable rocky terrain
     var id = 1;
+    var traversable = false;
     rocksU = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as rocky mountains
              new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/mountain.jpg')})
     );
@@ -25,6 +28,7 @@ function uTerrain(x, z){ //Untraversable rocky terrain
 
 function wTerrain(x, z){ //Untraversable water terrain
     var id = 2;
+    var traversable = false;
     waterU = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as water
              new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/water.jpg')})
     );
