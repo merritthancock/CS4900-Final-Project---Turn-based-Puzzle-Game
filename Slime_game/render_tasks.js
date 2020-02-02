9890d53c46075fd46bbade5421f5a6fbbe267057
canikostar99 <54637481+canikostar99@users.noreply.github.com>
2020-02-02 14:54:35 -0500

    update render tasks

--- Slime_game/render_tasks.js
+++ Slime_game/render_tasks.js
@@ -1,55 +1,46 @@
 
 function updateRender(){
     if(keyStatus["leftArrow"]){
-        camera.translateX(-0.3)	;
+        moveCameraLeft();
     }
     if(keyStatus["rightArrow"]){
-        camera.translateX(0.3);
+        moveCameraRight();
     }
     if(keyStatus["upArrow"]){
-        camera.translateY(0.3);
-        camera.translateZ(-0.3);  
+        moveCameraForward();
     }
     if(keyStatus["downArrow"]){
-        camera.translateY(-0.3);
-        camera.translateZ(0.3);
+        moveCameraBackward();
     }
     if(keyStatus["qKey"]){
-        camera.position.x = x * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
-        camera.position.z = z * Math.cos(rotSpeed) + x * Math.sin(rotSpeed);
-        camera.lookAt(scene.position);
+        rotateCameraLeft();
     }
     if(keyStatus["eKey"]){
-        camera.position.x = x * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
-        camera.position.z = z * Math.cos(rotSpeed) - x * Math.sin(rotSpeed);
-        camera.lookAt(scene.position);      
+        rotateCameraRight();      
     }
     if(keyStatus["oKey"]){
-        camera.translateZ(-0.3);     
-    }
-    
+        zoomCameraIn();     
+    }    
     if(keyStatus["pKey"]){
-        camera.translateZ(0.3);
+        zoomCameraOut();
     }
     
-    if(movementUnlocked){
-        if(keyStatus["wKey"]){
-            cursorForward();
+    
+        if(movementUnlocked){
+            if(keyStatus["wKey"]){
+                cursorForward();
+            }
+            if(keyStatus["aKey"]){
+                cursorLeft();
+            }
+            if(keyStatus["sKey"]){
+                cursorBackward();
+            }
+            if(keyStatus["dKey"]){
+                cursorRight();
+            }
+            if(keyStatus["enter"]){
+                followCursor();
+            }
         }
-        if(keyStatus["aKey"]){
-            cursorLeft();
-        }
-        if(keyStatus["sKey"]){
-            cursorBackward();
-        }
-        if(keyStatus["dKey"]){
-            cursorRight();
-        }
-        if(keyStatus["enter"]){
-            followCursor();
-        }
-        if(keyStatus["space"]){
-            moveEnemy();
-        }
-    }
 }
