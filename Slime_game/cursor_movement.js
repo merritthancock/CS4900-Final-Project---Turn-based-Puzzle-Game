f9040ff3cce9997d4abc6882c1523cb2e7026414
Merritt Hancock <merrittdhancock@gmail.com>
2020-02-08 11:16:02 -0500

    Refactored Cursor Movement class

--- Slime_game/cursor_movement.js
+++ Slime_game/cursor_movement.js
@@ -9,6 +9,10 @@ function cursorSet(x, y, z){
     cursor_currentPos = [x, y, z];
     cursor.position.set(x, y, z);
 }
+//ResetPosition
+function cursorReset(){
+    cursor.position.set(cursor_startPos[0], cursor_startPos[1], cursor_startPos[2]);
+}
 
 //Moves in desired direction (forward, backward, left, or right according to the xyz axis)
 function cursorMove(direction){
@@ -34,56 +38,4 @@ function cursorMove(direction){
         cursor_currentPos[1] = cursor_startPos[1] + heightMap[cursor_currentPos[0]][cursor_currentPos[2]];
         cursor.position.set(cursor_currentPos[0], cursor_currentPos[1], cursor_currentPos[2]);
     }
-}
-//Move Forward (deprecated)
-function cursorForward(){
-    if(movementUnlocked){
-        movementUnlocked = false;
-        cursor_currentPos[2] += 1
-        cursor.position.set(cursor_currentPos[0], cursor_currentPos[1], cursor_currentPos[2]);
-    }
-}
-
-//Move Backwards (deprecated)
-function cursorBackward(){
-    if(movementUnlocked){
-        movementUnlocked = false;
-        cursor_currentPos[2] -= 1;
-        cursor.position.set(cursor_currentPos[0], cursor_currentPos[1], cursor_currentPos[2]);
-    }
-}
-
-//Move Left (deprecated)
-function cursorLeft(){
-    if(movementUnlocked){
-        movementUnlocked = false;
-        cursor_currentPos[0] += 1;
-        cursor.position.set(cursor_currentPos[0], cursor_currentPos[1], cursor_currentPos[2]);
-    }
-}
-
-//Move Right (deprecated)
-function cursorRight(){
-    if(movementUnlocked){
-        movementUnlocked = false;
-        cursor_currentPos[0] -= 1;
-        cursor.position.set(cursor_currentPos[0], cursor_currentPos[1], cursor_currentPos[2]);
-    }
-}
-
-//ResetPosition
-function cursorReset(){
-    cursor.position.set(cursor_startPos[0], cursor_startPos[1], cursor_startPos[2]);
-}
-
-//Move Up
-function cursorUp(){
-    cursor_currentPos[1] += 1;
-    cursor.position.set(cursor_startPos[0], cursor_startPos[1], cursor_startPos[2]);
-}
-
-//Move Down
-function cursorDown(){
-    cursor_currentPos[1] -= 1;
-    cursor.position.set(cursor_startPos[0], cursor_startPos[1], cursor_startPos[2]);
 }
