a6b36efbbffeec0c6b0dcf0ef29727152f6148a9
Ryan Trull <retrull@valdosta.edu>
2020-02-09 16:04:59 -0500

    changed terrain generation to one single method

--- Slime_game/terrain.js
+++ Slime_game/terrain.js
@@ -18,11 +18,11 @@ function grassTerrain(x, height, z){
 function rockyTerrain(x, height, z){
     //id = 1;
     var traversable = false;
-    rocksT = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as rocky mountains
+    rocksT = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1), //a tile of normal that appears as rocky mountains
              new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/mountain.jpg')})
     );
-    rocksT.rotation.x -= Math.PI / 2;
-    rocksT.position.set(x, height, z);
+    //rocksT.rotation.y -= Math.PI / 2;
+    rocksT.position.set(x, height/2, z);//height/2 because y position refers to the center of the box
     scene.add(rocksT);
 }
 
@@ -46,4 +46,33 @@ function gapTerrain(x, height, z){ //Gap space that makes player fall (different
     gapT.rotation.x -= Math.PI / 2;
     gapT.position.set(x, height, z);
     scene.add(gapT);
+}
+
+function makeTerrain(x, height, z, id){//add new terrains here as needed
+    //var traversable = false;
+    var terrain;
+    switch(id){
+        case 0://grass
+            terrain = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1),
+                      new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/grass.jpg')}));
+            break;
+        case 1://rocky
+            terrain = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1),
+                      new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/mountain.jpg')}));            
+            break;
+        case 2://water
+            terrain = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1),
+                      new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/water.jpg')}));;
+            break;
+        case 3://gap
+            terrain = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1),
+                      new THREE.MeshBasicMaterial({ color: 0x000000}));
+            break;
+    }
+    if(height == 0){
+        height -= 1;
+    }
+    terrain.position.set(x, height/2, z);//height/2 because y position refers to the center of the box
+    scene.add(terrain);    
+
 }
