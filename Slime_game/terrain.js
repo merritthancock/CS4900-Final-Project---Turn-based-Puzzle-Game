a4135d7dcb0e470557e7b9416005753fe3d9f255
Ryan Trull <retrull@valdosta.edu>
2020-02-09 11:30:58 -0500

    modified all terrains to accept a height

--- Slime_game/terrain.js
+++ Slime_game/terrain.js
@@ -1,48 +1,49 @@
-function zTerrain(id){ //Zero terrain
-    var id = -1;
+function voidTerrain(){ //Zero terrain
+    //id = -1;
     var traversable = false;
 }
+//Void Terrain: id = -1
 
-function nTerrain(x, z){ //Normal terrain (no restrictions)
-    var id = 0;
+function grassTerrain(x, height, z){
+    //id = 0; 
     var traversable = true;
     grassT = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as grass
              new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/grass.jpg') })
     );
     grassT.rotation.x -= Math.PI / 2;
-    grassT.position.set(x, 0, z);
+    grassT.position.set(x, height, z);
     scene.add(grassT);
 }
 
-function uTerrain(x, z){ //Untraversable rocky terrain
-    var id = 1;
+function rockyTerrain(x, height, z){
+    //id = 1;
     var traversable = false;
-    rocksU = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as rocky mountains
+    rocksT = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as rocky mountains
              new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/mountain.jpg')})
     );
-    rocksU.rotation.x -= Math.PI / 2;
-    rocksU.position.set(x, 0, z);
-    scene.add(rocksU);
+    rocksT.rotation.x -= Math.PI / 2;
+    rocksT.position.set(x, height, z);
+    scene.add(rocksT);
 }
 
-function wTerrain(x, z){ //Untraversable water terrain
-    var id = 2;
+function waterTerrain(x, height, z){
+    //id = 2;
     var traversable = false;
     waterU = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as water
              new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./assets/water.jpg')})
     );
     waterU.rotation.x -= Math.PI / 2;
-    waterU.position.set(x, heightMap[x][z], z);
+    waterU.position.set(x, height, z);
     scene.add(waterU);
 }
 
-function gTerrain(x, z){ //Gap space that makes player fall (different from void space)
-    var id = -2;
+function gapTerrain(x, height, z){ //Gap space that makes player fall (different from void space)
+    //id = 3;
     var traversable = true;
     gapT = new THREE.Mesh(new THREE.PlaneGeometry(1,1), //a tile of normal that appears as a black space ( generates a few tiles down)
              new THREE.MeshBasicMaterial({ color: 0x000000})
     );
     gapT.rotation.x -= Math.PI / 2;
-    gapT.position.set(x, heightMap[x][z], z);
+    gapT.position.set(x, height, z);
     scene.add(gapT);
 }
