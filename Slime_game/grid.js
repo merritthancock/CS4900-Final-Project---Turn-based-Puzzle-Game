eca196f477920099a6e0980bd6dec465e5eb4296
Ryan Trull <retrull@valdosta.edu>
2020-02-09 11:38:06 -0500

    Cleaned up extraneous grid code

--- Slime_game/grid.js
+++ Slime_game/grid.js
@@ -43,62 +43,6 @@ function createLevel(){ //object function to create the grid for level 1
     }
 }
 
-//import { loader, loadLevel2 } from './imports.js';
-function createGrid2(){ //object function to create the grid for level 2
-    //Grid represents the world. A 1 means untraversable, 0 means traversable, -1 means empty space(hole)
-    grid = [ 
-        [-1, -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1, -1, -1],
-        [-1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1, -1, -1],
-        [ 1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1, -1, -1],
-        [ 1,  0,  0,  0,  1,  1,  0,  0,  0,  1,  1,  1,  1,  0,  0,  1, -1],
-        [ 1,  0,  0,  1,  2,  2,  1,  0,  0,  0, -2, -2,  1,  0,  0,  0,  1],
-        [ 1,  0,  0,  1,  2,  2,  1,  0,  0,  0, -2, -2,  1,  0,  0,  0,  1],
-        [ 1,  0,  0,  0,  1,  1,  0,  0,  0,  1,  1,  1,  1,  0,  0,  1, -1],
-        [ 1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1, -1, -1],
-        [-1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1, -1, -1],
-        [-1, -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1, -1, -1]
-    ];
-
-    //Temporary heightmap until tile object implemented
-    heightMap = [
-        [-1, -1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2, -1, -1, -1],
-        [-1,  2,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2, -1, -1],
-        [ 2,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2, -1, -1],
-        [ 2,  0,  0,  0,  2,  2,  0,  0,  0,  2,  2,  2,  2,  0,  0,  2, -1],
-        [ 2,  0,  0,  2,  2,  2,  2,  0,  0,  0,  0,  0,  2,  0,  0,  0,  2],
-        [ 2,  0,  0,  2,  2,  2,  2,  0,  0,  0,  0,  0,  2,  0,  0,  0,  2],
-        [ 2,  0,  0,  0,  2,  2,  0,  0,  0,  2,  2,  2,  2,  0,  0,  2, -1],
-        [ 2,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2, -1, -1],
-        [-1,  2,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2, -1, -1],
-        [-1, -1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2, -1, -1, -1]
-    ]
-
-    var start_pos = grid[2][2];
-
-    //populates the grid with each terrain type
-    for(r = 0; r< grid.length; r++){
-        for(c = 0; c< grid[r].length; c++){
-            if (grid[r][c] == 0){
-                //nTerrain(r,c); //calls from terrain.js
-                setGridOverlay(r,c);
-            }
-            if (grid[r][c] == 1){
-                //uTerrain(r,c);
-                setGridOverlay(r,c);
-            }
-            if (grid[r][c] ==2){
-                wTerrain(r,c);
-                setGridOverlay(r,c);
-            }
-            if (grid[r][c] == -2){
-                gTerrain(r,c);
-                setGridOverlay(r,c);
-            }
-        }
-    }
-}
-
-
 function checkGrid(cPosition, pPosition, distance){ //checks current position on grid to determine if spot is traversable or not
     //var rLen = grid[posX].length;
     var traversable = false;
