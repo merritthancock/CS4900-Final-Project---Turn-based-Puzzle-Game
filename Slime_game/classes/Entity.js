bf70c07237d485c28eecadc7998b61a78d943d28
Merritt Hancock <merrittdhancock@gmail.com>
2020-02-10 16:55:56 -0500

    Added more attributes to enemy

--- Slime_game/classes/Entity.js
+++ Slime_game/classes/Entity.js
@@ -95,9 +95,18 @@ class Cursor extends Entity {
 }
 //The Enemy is an object that will contain unique methods allowing player interaction
 class Enemy extends Entity {
-    constructor(position, model, texture, id){
+    constructor(position, model, texture, id, startingMass, startPriority){
         //Call entity constructor
         super(position, model, texture, id);
+
+        //Set starting mass
+        this.mass = startingMass;
+        //Set abilities to an empty set for starters
+        this.abilities = {};
+        //Set default movement range to 1
+        this.movementRange = 1;
+        //Set the priority of the enemy
+        this.priority = startPriority;
     }
 
     moveEnemy(direction){
@@ -118,5 +127,9 @@ class Enemy extends Entity {
         //Set height equal to the height of the tile that the cursor was moved over.
         /*this.position[1] = level.board[this.position[0]][this.position[2]].height;
         this.position.set(cursor_currentPos[0], cursor_currentPos[1], cursor_currentPos[2]);*/
+    }
+
+    useAbility(){
+        
     }
 }
