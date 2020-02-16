64d739f827ea4eb6f3d0311d7cc0558c692772b7
canikostar99 <54637481+canikostar99@users.noreply.github.com>
2020-02-15 20:36:29 -0500

    Trying to fix texture stretching
    Just an in progress commit. Changing boxgeometry to cylindergeometry fixes the texture stretch, but turns all boxes diagonal

--- Slime_game/classes/Board.js
+++ Slime_game/classes/Board.js
@@ -29,21 +29,32 @@ class Tile {
         
         switch(type){
             case 0://grass
-                this.terrain = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1),
+                this.terrain = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, height, 4, height),
                                new THREE.MeshBasicMaterial({ map: grass}));
                 break;
             case 1://rocky
-                this.terrain = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1),
+            this.terrain = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, height, 4, height, false, 0.5 * Math.PI),
                                new THREE.MeshBasicMaterial({ map: rocks}));            
                 break;
             /*case 2://water
-                this.terrain = new THREE.Mesh(new THREE.BoxGeometry(1, height, 1),
+                this.terrain = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, height, 4, height),
                                new THREE.MeshBasicMaterial({ map: water }));;
                 break;*/
             case 3://gap
                 this.terrain = new THREE.Mesh(new THREE.BoxGeometry(1, .1, 1),
                                new THREE.MeshBasicMaterial({ color: 0x000000}));
                 break;
+<<<<<<< Updated upstream
+=======
+            case 4://cave
+            this.terrain = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, height, 4, height),
+                               new THREE.MeshBasicMaterial({ map: cave}));            
+                break;
+            case 8://exit
+            this.terrain = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, height, 4, height),
+                               new THREE.MeshBasicMaterial({ color: 0xFADADD}));
+                break;
+>>>>>>> Stashed changes
             default:
                 this.terrain = null;
                 break;
