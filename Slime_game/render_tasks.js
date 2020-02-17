b009d835634a5e6f035e39c2084dbb553eb8121b
Merritt Hancock <merrittdhancock@gmail.com>
2020-02-17 09:21:21 -0500

    Added semaphores to cursor

--- Slime_game/render_tasks.js
+++ Slime_game/render_tasks.js
@@ -3,18 +3,22 @@ function updateRender(board){
     //console.log(board.cursor.id);
     if(movementUnlocked){
         if(keyStatus["wKey"]){
+            movementUnlocked = false;
             console.log("Moving cursor forward!");
             board.cursor.moveCursor(board.cursor, "forward");
         }
         if(keyStatus["aKey"]){
+            movementUnlocked = false;
             console.log("Moving cursor left!");
             board.cursor.moveCursor(board.cursor, "left");
         }
         if(keyStatus["sKey"]){
+            movementUnlocked = false;
             console.log("Moving cursor backward!");
             board.cursor.moveCursor(board.cursor, "backward");
         }
         if(keyStatus["dKey"]){
+            movementUnlocked = false;
             console.log("Moving cursor right!");
             board.cursor.moveCursor(board.cursor, "right");
         }
