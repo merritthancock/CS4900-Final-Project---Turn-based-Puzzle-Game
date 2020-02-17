777afeb877ae84b70eb330b3a05852ab40841976
Ryan Trull <retrull@valdosta.edu>
2020-02-17 13:02:11 -0500

    Cursor now reads the tile type and height

--- Slime_game/render_tasks.js
+++ Slime_game/render_tasks.js
@@ -6,21 +6,25 @@ function updateRender(board){
             movementUnlocked = false;
             console.log("Moving cursor forward!");
             board.cursor.moveCursor(board.cursor, "forward");
+            hover(board);
         }
         if(keyStatus["aKey"]){
             movementUnlocked = false;
             console.log("Moving cursor left!");
             board.cursor.moveCursor(board.cursor, "left");
+            hover(board);
         }
         if(keyStatus["sKey"]){
             movementUnlocked = false;
             console.log("Moving cursor backward!");
             board.cursor.moveCursor(board.cursor, "backward");
+            hover(board);
         }
         if(keyStatus["dKey"]){
             movementUnlocked = false;
             console.log("Moving cursor right!");
             board.cursor.moveCursor(board.cursor, "right");
+            hover(board);
         }
         if(keyStatus["enter"]){
             //followCursor();
@@ -29,4 +33,15 @@ function updateRender(board){
             //moveEnemy();
         }
     }
+    //hover(board);
+}
+
+function hover(board){
+    cPos = board.cursor.position;
+    var type = board.tileMap[cPos[0]][cPos[2]];
+    var height = board.heightMap[cPos[0]][cPos[2]];
+    console.log(type);
+    console.log(height);
+    //board.cursor.position = (cPos[0], height + 0.5, cPos[2]);
+    //return type;
 }
