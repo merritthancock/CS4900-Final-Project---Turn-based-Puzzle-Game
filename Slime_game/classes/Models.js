e783a9faa52e820f0db538280748a3d14936f947
canikostar99 <54637481+canikostar99@users.noreply.github.com>
2020-02-24 15:55:28 -0500

    Update Models.js

--- Slime_game/classes/Models.js
+++ Slime_game/classes/Models.js
@@ -1,48 +1,28 @@
+//https://medium.com/@matthewmain/how-to-import-a-3d-blender-object-into-a-three-js-project-as-a-gltf-file-5a67290f65f2 Part 4 is helpful
+
 var manager = new THREE.LoadingManager();
 
-var loader = new THREE.GLTFLoader(manager);
+
+
+var loader = new THREE.GLTFLoader().setPath( '../assets/GLTFModels/' );
+
 loader.load(
 	// resource URL
-	'../assets/GLTFModels/MilcapSoldier.glb',
+	'MilcapSoldier.glb',
 	// called when the resource is loaded
+	
 	function ( gltf ) {
 
-		scene.add( gltf.scene );
+		
+		milcapSoldier = gltf.scene;
+
 
 		//gltf.animations; // Array<THREE.AnimationClip>
 		//gltf.scene; // THREE.Scene
 		//gltf.scenes; // Array<THREE.Scene>
 		//gltf.cameras; // Array<THREE.Camera>
 		//gltf.asset; // Object
-
-	},
-	// called while loading is progressing
-	function ( xhr ) {
-
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-	},
-	// called when loading has errors
-	function ( error ) {
-
-		console.log( 'An error happened' );
-
-	}
-);
-loader.load(
-	// resource URL
-	'../assets/GLTFModels/SlimeMain.glb',
-	// called when the resource is loaded
-	function ( gltf ) {
-
-		scene.add( gltf.scene );
-
-		//gltf.animations; // Array<THREE.AnimationClip>
-		//gltf.scene; // THREE.Scene
-		//gltf.scenes; // Array<THREE.Scene>
-		//gltf.cameras; // Array<THREE.Camera>
-		//gltf.asset; // Object
-
+		scene.add( milcapSoldier );
 	},
 	// called while loading is progressing
 	function ( xhr ) {
