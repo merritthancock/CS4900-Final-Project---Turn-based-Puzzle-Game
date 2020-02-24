0a60cc7b90062d41ea0f1e0dffc0c84d36eecee9
Merritt Hancock <merrittdhancock@gmail.com>
2020-02-24 11:00:24 -0500

    Filled out moveCamera() method

--- Slime_game/classes/Camera.js
+++ Slime_game/classes/Camera.js
@@ -4,12 +4,16 @@ import {camera} from "./Controller.js"
 function moveCamera(camera, direction){
     switch(direction){
         case "forward":
+            camera.position.z += 1;
             break;
         case "backward":
+            camera.position.z -= 1;
             break;
         case "left":
+            camera.position.x -= 1;
             break;
         case "right":
+            camera.position.x += 1;
             break;
     }
 }
