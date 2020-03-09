f71af385454daa19c486eba2ccc2c697d885eecb
Merritt Hancock <merrittdhancock@gmail.com>
2020-03-09 15:15:08 -0400

    Made Level class

---
+++ Slime_game/classes/Level.js
@@ -0,0 +1,19 @@
+
+class Level{
+    constructor(heightMap, terrainMap, enemies, player, startPos){
+
+        //Set 2d heightMap
+        this.heightMap = heightMap;
+        //Set 2d terrainMap
+        this.terrainMap = terrainMap;
+        //Set enemies array
+        this.enemies = enemies;
+        //Set player
+        this.player = player;
+        //Set startPos
+        this.startPos = startPos;
+        
+    }
+}
+
+export{Level};
