1de0cff8b278416b777070f043bf99a2a20910b2
canikostar99 <54637481+canikostar99@users.noreply.github.com>
2020-03-10 12:07:01 -0400

    Update Models.js

--- Slime_game/classes/Models.js
+++ Slime_game/classes/Models.js
@@ -6,43 +6,7 @@ var manager = new THREE.LoadingManager();
 
 var loader = new THREE.GLTFLoader().setPath( '../../Slime_game/assets/GLTFModels/' );
 
-var milcapSoldier = loader.load(
-	// resource URL
-	'MilcapSoldier.glb',
-	// called when the resource is loaded
-	
-	function ( gltf ) {
-		scene.add(gltf.scene);
-		gltf.scene.scale.set(.5, .5, .5);
-		gltf.scene.position.set(15, 1, 3);
-	},
-	// called while loading is progressing
-	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	}
-);
 
-var cursor = loader.load(
-	// resource URL
-	'CursorOption2.glb',
-	// called when the resource is loaded
-	
-	function ( gltf ) {
-		scene.add(gltf.scene);
-		gltf.scene.scale.set(.5, .5, .5);
 
-		gltf.scene.position.set(6, 1, 2);
 
-		gltf.animations; // Array<THREE.AnimationClip>
-		gltf.scene; // THREE.Scene
-		gltf.scenes; // Array<THREE.Scene>
-		gltf.cameras; // Array<THREE.Camera>
-		gltf.asset; // Object
-
-	},
-	// called while loading is progressing
-	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	}
-);
 export {loader};
