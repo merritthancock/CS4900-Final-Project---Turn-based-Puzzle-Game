a32237166f744acf20867bf69c015c25fc6ebe60
Merritt Hancock <merrittdhancock@gmail.com>
2020-03-20 15:34:37 -0400

    Added css to menu screen

--- Slime_game/classes/Controller.js
+++ Slime_game/classes/Controller.js
@@ -27,36 +27,47 @@ function start(){
     //Level 1
     startButton.onclick = function(){
         //Sets current scene to level 1 scene
+        menu.style.display = "none";
+        console.log("Level 1");
         currentScene = scene;
-        //Creates renderer and adds it to document body
-        renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setSize(windowWidth, windowHeight);
-        document.body.appendChild(renderer.domElement);
-
-        //set listener for window resizing. Allows resizing of game.
-        window.addEventListener('resize', () => {
-            renderer.setSize(window.innerWidth,window.innerHeight);
-            camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
-        });
-
-        //Adds event listeners to document
-        document.addEventListener('keyup', doKeyUp, false);
-        document.addEventListener('keydown', doKeyDown, false);
-        //----------------------------------------------------------------
+        setupTasks();
         setupLevel1();
     };
     //Level 2
     level2Button.onclick = function(){
-        console.log("Level2");
+        console.log("Level 2");
+        currentScene = scene;
+        setupTasks();
+        setupLevel1();
     };
     //Level 3
     level3Button.onclick = function(){
         console.log("Level 3");
+        currentScene = scene;
+        setupTasks();
+        setupLevel1();
     };
-
 }
     
+function setupTasks(){
+    //Creates renderer and adds it to document body
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(windowWidth, windowHeight);
+    document.body.appendChild(renderer.domElement);
+
+    //set listener for window resizing. Allows resizing of game.
+    window.addEventListener('resize', () => {
+        renderer.setSize(window.innerWidth,window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    });
+
+    //Adds event listeners to document
+    document.addEventListener('keyup', doKeyUp, false);
+    document.addEventListener('keydown', doKeyDown, false);
+    //----------------------------------------------------------------
+}
+
 function setupLevel1(){
     camera = new THREE.PerspectiveCamera(45, windowWidth / windowHeight, 0.1, 10000);
     buildCamera();
@@ -98,7 +109,6 @@ function animate() {
     renderLevel();
 }
 start();
-//setupLevel();
 
 export {camera};
 export {cameraControls};
