272945ef327cc940d63ebea5b5595ee48ea9a2ce
Alan Bettis <alanbettis56@gmail.com>
2020-03-22 12:17:25 -0400

    Removed duplicate methods from Enemy.js

--- Slime_game/classes/Entities/Enemy.js
+++ Slime_game/classes/Entities/Enemy.js
@@ -1,8 +1,7 @@
 import {currentLevel} from "../LevelManager.js";
 import {Entity} from "./Entity.js";
-//import {FollowPathBehavior} from "../libraries/yuka-master/src/yuka.js";
-import {Path, PursuitBehavior} from "../../libraries/yuka-master/src/yuka.js";
-import {aStar, checkNeighbor} from "../Pathing.js";
+import {Path} from "../../libraries/yuka-master/src/yuka.js";
+import {aStar} from "../Pathing.js";
 
 //The Enemy is an object that will contain unique methods allowing player interaction
 class Enemy extends Entity {
@@ -13,17 +12,17 @@ class Enemy extends Entity {
         this.mass = startingMass;
         //Set abilities to an empty set for starters
         this.abilities = {};
-        //Set default movement range to 1
-        this.movementRange = 2;
-        //Set jump height to 1
-        this.jumpHeight = 1;
         //Set the priority of the enemy
         this.priority = startPriority;
         //Set the enemy's range of vision for seeing the player
         this.visionRange = visionRange;
-        //the damage the enemy deals on an attack
+        //Set default attack stats
         this.attackPower = 0.5;
         this.attackRange = 1;
+        //Set default movement range to 1
+        this.movementRange = 2;
+        //Set default jump height to 1
+        this.jumpHeight = 1;
 
         //Give the enemy a path to patrol (loop must be set to true if path is cyclical)
         this.path = new Path();
@@ -46,10 +45,6 @@ class Enemy extends Entity {
                 this.position[0] -= 1;
                 break;
         }
-    }
-
-    useAbility(){
-        
     }
 
     //Checks if the player is within sight range
@@ -117,7 +112,6 @@ class Enemy extends Entity {
     attack(damage){
         //Play attack animation
         currentLevel.player.takeDamage(damage);
-
     }
     
     //set custom attack power for enemy type
@@ -131,48 +125,8 @@ class Enemy extends Entity {
     }
 
     //set custom attack range
-    setARange(newAR){
+    setAttackRange(newAR){
         this.attackRange = newAR;
-    }
-
-    //Moves the enemy in the direction of the player
-    moveToPlayer(){
-        let pos = currentLevel.player.position;
-
-        aStar(this.position[0], this.position[2], pos[0], pos[2], currentLevel.board, this);
-
-        console.log(this.path);
-        console.log(this.mesh.position);
-    }
-
-    loop(){//changes whether path can loop
-        if(this.path.loop == true){
-            this.path.loop = false;
-        }
-        else{
-            this.path.loop = true;
-        }
-    }
-
-    pathAdd(waypoint){//add new waypoint to enemy patrol path
-        this.path.add(waypoint);
-    }
-
-    //damage the playerh
-    attack(damage){
-        //Play attack animation
-        currentLevel.player.takeDamage(damage);
-
-    }
-    
-    //set custom attack power for enemy type
-    setAttackPower(atPow){
-        this.attackPower = atPow;
-    }
-
-    //set custom enemy mass for enemy type
-    setMass(newMass){
-        this.mass = newMass;
     }
 }
 export {Enemy};
