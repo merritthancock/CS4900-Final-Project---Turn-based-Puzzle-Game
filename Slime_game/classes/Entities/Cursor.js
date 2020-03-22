79ecf1e47b9e5b1ca69193f304cd808373a8d025
Alan Bettis <alanbettis56@gmail.com>
2020-03-22 12:07:30 -0400

    Cleaned Cursor.js

--- Slime_game/classes/Entities/Cursor.js
+++ Slime_game/classes/Entities/Cursor.js
@@ -22,41 +22,11 @@ class Cursor extends Entity {
                 break;
         }
 
-        
         cursor.mesh.position.set(this.position[0], this.position[1], this.position[2]);
-        //Set height equal to the height of the tile that the cursor was moved over.
-        //this.position[1] = level.board[this.position[0]][this.position[2]].height;
     }
     cursorHeight(cursor, height){
         cursor.mesh.position.set(this.position[0], height + 1, this.position[2]);
-        //this.position[1] = height;
     }
-    /* deprecated method (for reference)
-        function cursorMove(direction){
-            //Check if movement of the cursor is locked
-            if(movementUnlocked){
-                //If movement is unlocked, lock the movement so that the cursor is not useable while movement takes place
-                movementUnlocked = false;
-                
-                switch(direction){
-                    case "forward":
-                        cursor_currentPos[2] += 1
-                        break;
-                    case "backward":
-                        cursor_currentPos[2] -= 1;
-                        break;
-                    case "left":
-                        cursor_currentPos[0] += 1;
-                        break;
-                    case "right":
-                        cursor_currentPos[0] -= 1;
-                        break;
-                }
-                cursor_currentPos[1] = cursor_startPos[1] + heightMap[cursor_currentPos[0]][cursor_currentPos[2]];
-                cursor.position.set(cursor_currentPos[0], cursor_currentPos[1], cursor_currentPos[2]);
-            }
-        }
-        */
 }
 
 export {Cursor};
