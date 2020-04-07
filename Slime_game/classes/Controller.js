2c62c99483594ef00832d8b225e9d1b0bb6fe0ef
Merritt Hancock <merrittdhancock@gmail.com>
2020-04-07 19:11:01 -0400

    fixed bug for win screen

--- Slime_game/classes/Controller.js
+++ Slime_game/classes/Controller.js
@@ -7,6 +7,7 @@ import {scene, testLevel} from "./LevelManager.js";
 //import {testLevel2} from "./LevelManager.js";
 import {loadLevel} from "./LevelManager.js";
 import {currentLevel, changeLevel} from "./Global.js";
+import { NavNode } from "../libraries/yuka-master/src/yuka.js";
 
 // declare variables
 let windowWidth;
@@ -29,9 +30,9 @@ let loadingScreen = document.getElementById("loading-screen");
 windowWidth = window.innerWidth;
 windowHeight = window.innerHeight;
 
-
 menuBtn.onclick = function(){
     winScreen.style.display = "none";
+    winScreen.style['pointer-events'] = 'none';
     loadingScreen.style.display = "block";
     canvas.style.display = "none";
     while(currentScene.children.length > 0) {
@@ -40,6 +41,7 @@ menuBtn.onclick = function(){
     }
     loadingScreen.style.display = "none";
     menu.style.display = "block";
+  
 };
 
 function start(){
@@ -111,6 +113,7 @@ function setupLevel(){
 }
 
 function winLevel(){
+    winScreen.style['pointer-events'] = 'auto';
     winScreen.style.opacity = 1;
 }
 
